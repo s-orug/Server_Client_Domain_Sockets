01a80b4101b699328f20b4789ff1a246195c8eeb
sai-oruganti <orugants@email.sc.edu>
2022-03-17 15:53:26 -0400

    Need INVALID FILE

--- text-server.c
+++ text-server.c
@@ -8,18 +8,6 @@
 #include <sys/un.h>
 #include <unistd.h>
 
-char *text_parse(char *word, char *file) {
-  FILE *fp;
-  size_t one = 0;
-  char command[1024], *line = NULL, *store = "";
-  sprintf(command, "grep \"%s\" %s ", word, file);
-  fp = popen(command, "r");
-  while (getline(&line, &one, fp) > 1) {
-    asprintf(&store, "%s%s", (char *const)store, (char *const)line);
-  }
-  return store;
-}
-
 int main(int argc, char *argv[]) {
   int server_sockfd, client_sockfd, n;
   struct sockaddr_un server_addr;
@@ -42,23 +30,40 @@ int main(int argc, char *argv[]) {
   listen(server_sockfd, n);
   fprintf(stderr, "SERVER STARTED\n\tMAX CLIENTS:%d\n", n);
   while (1) {
-    int client_len = sizeof client_addr;
-    char buff[256], tmp[512];
+    int client_len = sizeof client_addr, total = 0;//, size=0;
+    char buff[512], buff2[256], tmp[2048], *misc, path[512], seeking[512];
     client_sockfd =
         accept(server_sockfd, (struct sockaddr *)&client_addr, &(client_len));
     fprintf(stderr, "CLIENT CONNECTED\n");
 
-
     int k = read(client_sockfd, buff, sizeof(buff)); // receives path
-    buff[k]= '\0';
-    sprintf(tmp, "PATH: \"%s\"\n", buff);
-    fprintf(stderr, "%s", tmp);
+    buff[k] = '\0';
+    sprintf(path, "%s", buff);
+    fprintf(stderr, "PATH: \"%s\"\n", path);
+    fflush(stdout);
 
-    k = read(client_sockfd, buff, sizeof(buff)); // receives text
-    buff[k]= '\0';
-    sprintf(tmp, "SEEKING: \"%s\"\n", buff);
-    fprintf(stderr, "%s", tmp);
+    k = read(client_sockfd, buff2, sizeof(buff2)); // receives text
+    buff2[k] = '\0';
+    sprintf(seeking, "%s", buff2);
+    fprintf(stderr, "SEEKING: \"%s\"\n", seeking);
+    fflush(stdout);
 
+
+    if (strlen(seeking) != 0) {
+      FILE *fp;
+      int num_bytes=0;
+      char command[2048], line[2048], *store = "";
+      sprintf(command, "grep \"%s\" %s ", seeking, path);
+      fp = popen(command, "r");
+      while (fgets(line, sizeof(line) - 1, fp) != NULL) {
+        sprintf(tmp, "%s", line);
+        write(client_sockfd, tmp, sizeof(tmp));
+        num_bytes += strlen(tmp);
+      }
+      pclose(fp);
+      fprintf(stderr, "BYTES SENT: %d\n", num_bytes);
+      close(client_sockfd);
+    }
     close(client_sockfd);
   }
   return 0;
