47674245c988cb4b6fb3f9c200e0a815f0be91b3
sai-oruganti <orugants@email.sc.edu>
2022-03-17 18:43:50 -0400

    invalid file and simple client

--- text-client.c
+++ text-client.c
@@ -5,29 +5,29 @@
 #include <sys/types.h>
 #include <sys/un.h>
 #include <unistd.h>
- 
+
 int main(int argc, char *argv[]) {
 
   struct sockaddr_un address;
-  int reading, num_bytes = 0, i=1;
+  int reading, num_bytes = 0, i = 1;
   int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
   char tmp[2048];
   address.sun_family = AF_UNIX;
   strncpy(address.sun_path, argv[1], sizeof(address.sun_path) - 1);
   int rv = connect(sockfd, (struct sockaddr *)&address, sizeof address);
 
-  write(sockfd, argv[2], 512);//sends path
-  write(sockfd, argv[3], 512);//sends text
+  write(sockfd, argv[2], 512); // sends path
+  write(sockfd, argv[3], 512); // sends text
   
-  while(reading = read(sockfd, tmp, sizeof(tmp))){
-    if(strlen(tmp) > 0 && strlen(argv[3]) > 0){
-    num_bytes += strlen(tmp);
-    fprintf(stdout, "%d\t%s", i, tmp);
-    memset(tmp, '\0', sizeof(tmp));
-    i++;
+  while (reading = read(sockfd, tmp, sizeof(tmp))) {
+    if (strlen(tmp) > 0 && strlen(argv[3]) > 0) {
+      num_bytes += strlen(tmp);
+      fprintf(stdout, "%d\t%s", i, tmp);
+      memset(tmp, '\0', sizeof(tmp));
+      i++;
     }
   }
-  fprintf(stderr,"\nBYTES RECEIVED: %d\n", num_bytes);
+  fprintf(stderr, "\nBYTES RECEIVED: %d\n", num_bytes);
   close(sockfd);
   exit(0);
 }
--- text-server.c
+++ text-server.c
@@ -1,4 +1,5 @@
-#define _GNU_SOURCE 1
+#include <setjmp.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,6 +9,19 @@
 #include <sys/un.h>
 #include <unistd.h>
 
+jmp_buf savebuf;
+
+#define TRY if (setjmp(savebuf) == 0)
+#define CATCHALL else
+
+bool file_checker(char filename[]) {
+  FILE *f;
+  if (f = fopen(filename, "r")) {
+    fclose(f);
+    return true; // returns true if it can open
+  }
+  return false; // returns false if it can't open
+}
 int main(int argc, char *argv[]) {
   int server_sockfd, client_sockfd, n;
   struct sockaddr_un server_addr;
@@ -30,7 +44,7 @@ int main(int argc, char *argv[]) {
   listen(server_sockfd, n);
   fprintf(stderr, "SERVER STARTED\n\tMAX CLIENTS:%d\n", n);
   while (1) {
-    int client_len = sizeof client_addr, total = 0;//, size=0;
+    int client_len = sizeof client_addr, num_bytes = 0, total = 0; // size=0;
     char buff[512], buff2[256], tmp[2048], *misc, path[512], seeking[512];
     client_sockfd =
         accept(server_sockfd, (struct sockaddr *)&client_addr, &(client_len));
@@ -42,27 +56,47 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "PATH: \"%s\"\n", path);
     fflush(stdout);
 
+    if (file_checker(path) == 0) {
+      num_bytes = 0;
+      strcpy(tmp, "INVALID FILE AND NO TEXT\n");
+      write(client_sockfd, tmp, sizeof(tmp));
+      num_bytes += strlen(tmp);
+      fprintf(stderr, "BYTES SENT: %d\n", num_bytes);
+      close(client_sockfd);
+    }
+
     k = read(client_sockfd, buff2, sizeof(buff2)); // receives text
     buff2[k] = '\0';
     sprintf(seeking, "%s", buff2);
     fprintf(stderr, "SEEKING: \"%s\"\n", seeking);
     fflush(stdout);
 
-
     if (strlen(seeking) != 0) {
       FILE *fp;
-      int num_bytes=0;
       char command[2048], line[2048], *store = "";
       sprintf(command, "grep \"%s\" %s ", seeking, path);
+      // TRY {
       fp = popen(command, "r");
+      if (fp == NULL) {
+        printf("errrrrr\n");
+      }
       while (fgets(line, sizeof(line) - 1, fp) != NULL) {
         sprintf(tmp, "%s", line);
         write(client_sockfd, tmp, sizeof(tmp));
         num_bytes += strlen(tmp);
       }
       pclose(fp);
+      /*}
+      CATCHALL {
+        num_bytes = 0;
+        strcpy(tmp, "INVALID FILE AND NO TEXT\n");
+        write(client_sockfd, tmp, sizeof(tmp));
+        num_bytes += strlen(tmp);
+      }*/
       fprintf(stderr, "BYTES SENT: %d\n", num_bytes);
       close(client_sockfd);
+    } else {
+      fprintf(stderr, "BYTES SENT: %d\n", num_bytes);
     }
     close(client_sockfd);
   }
