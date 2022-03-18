1ac85b670f14dd25274c9e1653f628bebbecfb40
sai-oruganti <orugants@email.sc.edu>
2022-03-18 01:04:09 -0400

    final submission

--- text-client.c
+++ text-client.c
@@ -9,7 +9,7 @@
 int main(int argc, char *argv[]) {
 
   struct sockaddr_un address;
-  int reading, num_bytes = 0, i = 1;
+  int reading, num_bytes = 0, i = 0;
   int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
   char tmp[2048];
   address.sun_family = AF_UNIX;
@@ -24,15 +24,22 @@ int main(int argc, char *argv[]) {
   write(sockfd, argv[3], 512); // sends text
 
   while (reading = read(sockfd, tmp, sizeof(tmp))) {
-    fprintf(stderr, "SERVER CONNECCTION ACCEPTED\n");
+    if (i == 0) { // if it is the first itereation,
+                  // then the server accepted so its written to stdout
+      fprintf(stderr, "SERVER CONNECTION ACCEPTED\n");
+      i++;
+    }
     if (strlen(tmp) > 0 && strlen(argv[3]) > 0) {
       num_bytes += strlen(tmp);
-      fprintf(stdout, "%d\t%s", i, tmp);
+      fprintf(stdout, "%d\t%s", i,
+              tmp); // prints out each line in a formatted manner
       memset(tmp, '\0', sizeof(tmp));
       i++;
     }
   }
-  fprintf(stderr, "\nBYTES RECEIVED: %d\n", num_bytes);
-  close(sockfd);
+
+  fprintf(stderr, "\nBYTES RECEIVED: %d\n",
+          num_bytes); // logging number of bytes received
+  close(sockfd);      // closes the socket file descriptor
   exit(0);
 }
--- text-server.c
+++ text-server.c
@@ -9,7 +9,7 @@
 #include <sys/un.h>
 #include <unistd.h>
 
-bool file_checker(char filename[]) {
+bool file_checker(char filename[]) { // used to check if a file can be opened
   FILE *f;
   if (f = fopen(filename, "r")) {
     fclose(f);
@@ -28,6 +28,7 @@ int main(int argc, char *argv[]) {
   } else {
     n = get_nprocs_conf();
   }
+  // gets the number of processes
 
   unlink(argv[1]);
   server_sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
@@ -39,41 +40,47 @@ int main(int argc, char *argv[]) {
 
   listen(server_sockfd, n);
   fprintf(stderr, "SERVER STARTED\n\tMAX CLIENTS:%d\n", n);
+  // logging that the server started and max clients allowed
+
   while (1) {
-    int client_len = sizeof client_addr, num_bytes = 0, total = 0; // size=0;
-    char buff[512], buff2[256], tmp[2048], *misc, path[512], seeking[512];
+    int client_len = sizeof client_addr, num_bytes = 0;
+    char buff[512], buff2[256], tmp[2048], path[512], seeking[512];
     client_sockfd =
         accept(server_sockfd, (struct sockaddr *)&client_addr, &(client_len));
-    fprintf(stderr, "CLIENT CONNECTED\n");
+    fprintf(stderr, "CLIENT CONNECTED\n"); // logging a new client connection
 
     int k = read(client_sockfd, buff, sizeof(buff)); // receives path
     buff[k] = '\0';
     sprintf(path, "%s", buff);
-    fprintf(stderr, "PATH: \"%s\"\n", path);
+    fprintf(stderr, "PATH: \"%s\"\n", path); // logging the path received
     fflush(stdout);
 
     k = read(client_sockfd, buff2, sizeof(buff2)); // receives text
     buff2[k] = '\0';
     sprintf(seeking, "%s", buff2);
-    fprintf(stderr, "SEEKING: \"%s\"\n", seeking);
+    fprintf(stderr, "SEEKING: \"%s\"\n", seeking); // logging the text received
     fflush(stdout);
 
-    if (file_checker(path) < 1) {
+    if (file_checker(path) < 1) { // checks if the file path is valid
+      memset(tmp, '\0', sizeof(tmp));
       num_bytes = 0;
       strcpy(tmp, "INVALID FILE\n");
-      write(client_sockfd, tmp, sizeof(tmp));
+      write(client_sockfd, tmp,
+            sizeof(tmp)); // if the above argument fails
+                          // INVALID file is written to the client
       num_bytes += strlen(tmp);
-      close(client_sockfd);
     }
 
     if (strlen(seeking) != 0) {
+      memset(tmp, '\0', sizeof(tmp));
       FILE *fp;
       char command[2048], line[2048], *store = "";
       sprintf(command, "grep \"%s\" %s ", seeking, path);
-      if (file_checker(path) != 0) {
+      if (file_checker(path) != 0) { // if the path is valid and text is present
         fp = popen(command, "r");
 
         while (fgets(line, sizeof(line) - 1, fp) != NULL) {
+          // the loop gets each line from grep and sends it to the client
           sprintf(tmp, "%s", line);
           write(client_sockfd, tmp, sizeof(tmp));
           num_bytes += strlen(tmp);
@@ -83,15 +90,16 @@ int main(int argc, char *argv[]) {
         pclose(fp);
       }
 
-      fprintf(stderr, "BYTES SENT: %d\n", num_bytes);
-      close(client_sockfd);
+      fprintf(stderr, "BYTES SENT: %d\n",
+              num_bytes); // logging number of bytes sent
     } else {
       if (strlen(seeking) == 0) {
         num_bytes = 0;
       }
-      fprintf(stderr, "BYTES SENT: %d\n", num_bytes);
+      fprintf(stderr, "BYTES SENT: %d\n",
+              num_bytes); // logging number of bytes sent
     }
-    close(client_sockfd);
+    close(client_sockfd); // closes the client socket file descriptor
   }
   return 0;
 }
